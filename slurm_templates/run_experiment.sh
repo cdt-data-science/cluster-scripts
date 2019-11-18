9673be616027cd40528bf17d56c8d8d5978fdf67
JamesOwers <james.f.owers+github@gmail.com>
2019-11-18 12:17:14 +0000

    Changed script to take args and require bash script
    Decided that, since the bash script will likely require changing
    for each project, it's best to require this to be specified. Also
    implemented opt val arguments because enforcing position would def
    cause issues in future.

--- slurm_templates/run_experiment.sh
+++ slurm_templates/run_experiment.sh
@@ -3,77 +3,104 @@
 
 print_usage () {
   cat << EOM
-Usage: $0 experiment_textfile [max_nr_parallel_jobs [sbatch_args]]
-    Convenience function for running arrayjob experiments with slurm. Expects
-    the user to have created a bash script slurm_arrayjob.sh which is on the
-    path, and a text file with each line containing the command to run an
-    experiment. See slurm_arrayjob.sh.template adjacent to this script and
-    ./example/slurm_arrayjob.sh to help you create your own.
+Usage: $0 -t slurm_template -e experiment_textfile [-m max_nr_parallel_jobs] [sbatch_args]
+  Convenience function for running arrayjob experiments with slurm. Expects
+  the user to have created a bash script to supply to sbatch, and a text file
+  with each line containing a command to run a single experiment. For a filled
+  out example of a bash script to supply to sbatch, and an experiment file,
+  see the directory ./example adjacent to this with its README.md. You can use
+  slurm_arrayjob.sh.template adjacent to this script to help you create your
+  own too.
 
-Arguments (all poisitional):
-    1. experiment_textfile : str
-        a file which contains a command to run on each line
-    2. (optional) max_nr_parallel_jobs : int
-        the maximum number of jobs that can run simultaneously. If not
-        not specified, defaults to 10
-    3. (optional) sbatch_arg : str
-        all remaining arguments will be combined and supplied to the sbatch
-        command being executed. N.B. you *must* specify max_nr_parallel_jobs
-        to use this feature
-        
+Arguments:
+
+  -t slurm_template : str
+     the path to the bash script to supply to sbatch
+     
+  -e experiment_textfile : str
+     a file which contains a command to run on each line
+     
+  -m (optional) max_nr_parallel_jobs : int
+     the maximum number of jobs that can run simultaneously. If not
+     not specified, defaults to 10
+     
+  all remaining args (optional) sbatch_arg : str
+     all remaining arguments will be combined and supplied to the sbatch
+     command being executed. N.B. you *must* specify max_nr_parallel_jobs
+     to use this feature
+    
 Example call:
-    $ run_experiment.sh example/experiments.txt 12 --partition=PGR-Standard --mem=8000
+  $ run_experiment.sh -t example/slurm_arrayjob.sh -e example/experiments.txt -m 12 --gres=gpu:1 --mem=8000
 
 EOM
 }
 
-# Check slurm_arrayjob.sh on $PATH
-if ! [ -x "$(command -v slurm_arrayjob.sh)" ]; then
-    echo "You must create slurm_arrayjob.sh and add it to your PATH." >&2
-    echo "${#@}"
-    print_usage
-    exit 1
+BASH_SCRIPT=""
+EXPT_FILE=""
+MAX_PARALLEL_JOBS=10
+
+# Parse args
+while getopts 't:e:m:' flag; do
+  case "${flag}" in
+    t) BASH_SCRIPT="${OPTARG}" ;;
+    e) EXPT_FILE="${OPTARG}" ;;
+    m) MAX_PARALLEL_JOBS="${OPTARG}" ;;
+    *) print_usage
+       exit ;;
+  esac
+done
+
+# Set all remaining args to SBATCH_ARGS (supplied direct to sbatch)
+shift $((OPTIND -1))
+SBATCH_ARGS="${@}"
+
+# Check a bash script supplied
+if [ -z "${BASH_SCRIPT}" ]; then
+  echo "You give a bash script to supply to the sbatch command."
+  echo "${#@}"
+  print_usage
+  exit 1
+fi
+
+# Check bash script exists
+if [ ! -f "$BASH_SCRIPT" ]; then
+  echo "${BASH_SCRIPT} does not exist"
+  echo "${#@}"
+  print_usage
+  exit 1
 fi
 
 # Check an experiment file supplied
-if [ ${#@} -lt 1 ]; then
-    echo "You must supply a file of experiment commands to run."
-    echo "${#@}"
-    print_usage
-    exit 1
+if [ -z "${EXPT_FILE}" ]; then
+  echo "You give a file of experiment commands to run (supplied to bash script)."
+  echo "${#@}"
+  print_usage
+  exit 1
 fi
 
 # Check experiment file exists
-EXPT_FILE=$1
 if [ ! -f "$EXPT_FILE" ]; then
-    echo "${EXPT_FILE} does not exist"
-    echo "${#@}"
-    print_usage
-    exit 1
+  echo "${EXPT_FILE} does not exist"
+  echo "${#@}"
+  print_usage
+  exit 1
 fi
 
 # Check/set maximum number of parallel jobs to run
-if [ $# == 1 ]; then
-    echo 'No max nr of parallel jobs provided, setting to 10'
-    MAX_PARALLEL_JOBS=10 
-else
-    MAX_PARALLEL_JOBS=$2
-    re='^[0-9]+$'
-    if ! [[ $MAX_PARALLEL_JOBS =~ $re ]] ; then
-        echo "The second argument is the max nr of parallel jobs"
-        echo "It must be a number, you supplied ${MAX_PARALLEL_JOBS}"
-        echo "${#@}"
-        print_usage
-        exit 1
-    fi
+re='^[0-9]+$'
+if ! [[ $MAX_PARALLEL_JOBS =~ $re ]] ; then
+  echo "The second argument is the max nr of parallel jobs"
+  echo "It must be a number, you supplied ${MAX_PARALLEL_JOBS}"
+  echo "${#@}"
+  print_usage
+  exit 1
 fi
 
 # Set the value for sbatch's --array argument
-SBATCH_ARGS="${@:3}"
 NR_EXPTS=`cat ${EXPT_FILE} | wc -l`
 echo "Executing command: "\
-     "sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS}"\
-              "$SBATCH_ARGS slurm_arrayjob.sh $EXPT_FILE"
+   "sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS}"\
+        "$SBATCH_ARGS $BASH_SCRIPT $EXPT_FILE"
 
 # Run the sbatch command
-sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} $SBATCH_ARGS slurm_arrayjob.sh $EXPT_FILE
+sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} $SBATCH_ARGS $BASH_SCRIPT $EXPT_FILE
