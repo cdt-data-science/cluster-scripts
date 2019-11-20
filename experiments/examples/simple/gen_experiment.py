4f13370f472a170c4aa274834701e1fd7750346f
JamesOwers <james.f.owers+github@gmail.com>
2019-11-20 10:29:30 +0000

    First draft gen expt script

--- experiments/examples/simple/gen_experiment.py
+++ experiments/examples/simple/gen_experiment.py
@@ -1,59 +1,34 @@
-#!/usr/bin/env python
-"""
-
-"""
+#!/usr/bin/env python3
+"""Script for generating experiments.txt"""
 import os
 
-task_nr = 1
-fmt = 'command'
-high_level_expt_name = f'task{task_nr}'
-out_dir = f'{code_dir}/output/{high_level_expt_name}'
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-in_dir = '/disk/scratch/s0816700/data/acme'
+# The home dir on the node's scratch disk
+USER = os.getenv('USER')
+# This may need changing to e.g. /disk/scratch_fast depending on the cluster
+SCRATCH_DISK = '/disk/scratch'  
+SCRATCH_HOME = f'{SCRATCH_DISK}/{USER}'
 
-epochs = 10000
-early_stopping = 50
-base_call = (
-    f"python {code_dir}/baselines/train_task.py "
-    f"--task {task_nr} "
-    f"--input {in_dir} "
-    f"--format {fmt} "
-    f"--seq_len 1000 "
-    f"--epochs {epochs} "
-    f"--batch_log_freq None "
-    f"--early_stopping {early_stopping} "
-)
+DATA_HOME = f'{SCRATCH_HOME}/simple/data'
+base_call = f"python3 train.py -i {DATA_HOME}/input -o {DATA_HOME}/output"
 
-nr_repeats = 3
-learning_rates = [1e-6, 1e-5, 1e-4]
-weight_decays = [1e-6, 1e-5]
-hiddens = [100, 250]
-nr_expts = nr_repeats * len(learning_rates) * len(weight_decays) * len(hiddens)
+learning_rates = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
+weight_decays = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
+nr_expts = len(learning_rates) * len(weight_decays)
 
 nr_servers = 10
 avg_expt_time = 60  # mins
 print(f'Total experiments = {nr_expts}')
 print(f'Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs')
 
-settings = [(lr, wd, hid, repeat) for lr in learning_rates 
-            for wd in weight_decays for hid in hiddens 
-            for repeat in range(nr_repeats)]
+settings = [(lr, wd) for lr in learning_rates for wd in weight_decays]
 
-output_file = open(f"{code_dir}/slurm/experiments/"
-                   f"{high_level_expt_name}_experiments.txt", "w")
-for lr, wd, hid, repeat in settings:
-    expt_name = f"{high_level_expt_name}__{lr}_{wd}_{hid}_{repeat}"
-    log_file = f"{out_dir}/{expt_name}.log"
-    model_outpath = f"{out_dir}/{expt_name}.checkpoint"
-    
+output_file = open("experiments.txt", "w")
+
+for lr, wd in settings:    
     expt_call = (
         f"{base_call} "
         f"--lr {lr} "
-        f"--weight_decay {wd} "
-        f"--hidden {hid} "
-        f"--output {model_outpath} "
-        f"--log_file {log_file}"
+        f"--weight_decay {wd}"
     )
     print(expt_call, file=output_file)
 
