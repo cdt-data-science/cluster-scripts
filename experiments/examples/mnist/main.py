a38fac7a69390b47f32d1a7a37125b0b63f59ea3
JamesOwers <james.f.owers+github@gmail.com>
2019-11-21 11:01:47 +0000

    Add logging to a file

--- experiments/examples/mnist/main.py
+++ experiments/examples/mnist/main.py
@@ -34,10 +34,15 @@ class Net(nn.Module):
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    train_loss = 0
+    correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
+        train_loss += F.nll_loss(output, target, reduction='sum').item()
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
@@ -45,6 +50,10 @@ def train(args, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+    
+    train_loss /= len(train_loader.dataset)
+    accuracy = correct / len(train_loader.dataset)
+    return train_loss, accuracy
 
 
 def test(args, model, device, test_loader):
@@ -64,6 +73,9 @@ def test(args, model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    
+    accuracy =  correct / len(test_loader.dataset)
+    return test_loss, accuracy
 
 
 def main():
@@ -118,15 +130,20 @@ def main():
 
     model = Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
+    
+    log_fh = open(f'{args.output}/{model_name}.log', 'w')
+    print('trn_loss,trn_acc,vld_loss,vld_acc', file=log_fh)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        loss, acc = train(args, model, device, train_loader, optimizer, epoch)
+        vld_loss, vld_acc = test(args, model, device, test_loader)
+        print(f'{loss},{acc},{vld_loss},{vld_acc}', file=log_fh)
         scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), f"{args.output}/{model_name}.pt")
+    
+    log_fh.close()
 
 if __name__ == '__main__':
     main()
